4a8003b14758177b1672c3910b557154c6dae6f5
Arpana Kumal <kumalarpana@gmail.com>
2024-11-12 19:36:18 +0545

    Added product page, product details and shopping cart. 1. Added mini toggle for Menu(Responsive).

---
+++ js/product.js
@@ -0,0 +1,15 @@
+var productImg = document.getElementById("productImg")
+var SmallImg = document.getElementsByClassName("small-img");
+
+SmallImg[0].onclick = function () {
+    productImg.src = SmallImg[0].src;
+}
+SmallImg[1].onclick = function () {
+    productImg.src = SmallImg[1].src;
+}
+SmallImg[2].onclick = function () {
+    productImg.src = SmallImg[2].src;
+}
+SmallImg[3].onclick = function () {
+    productImg.src = SmallImg[3].src;
+}
