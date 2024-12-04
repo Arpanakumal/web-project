476ed00fb48ced9602819f07e814191f63aff5b3
Arpana Kumal <kumalarpana@gmail.com>
2024-12-04 22:52:30 +0545

    Added Admin dashboard and neccessary pages for Admin Panel.

---
+++ src/features/admin/php/manage_admin.php
@@ -0,0 +1,115 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../../common/css/1.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link rel="stylesheet" href="../css/admin.css">
+    <link rel="stylesheet" href="../css/cat.css">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <title>Home page</title>
+</head>
+
+<body>
+    <div class="header">
+        <div class="container">
+            <div class="navbar">
+                <div class="logo">
+                    <img src="../../home/images/logo.png" width="200px">
+                </div>
+                <nav>
+                    <ul id="MenuItems">
+                        <li><a href="../php/admin_homepage.php">Home</a></li>
+                        <li><a href="../php/manage_admin.php">Admin</a></li>
+                        <li><a href="../php/manage_product.php">Products</a></li>
+                        <li><a href="../php/manage_cat.php">Category</a></li>
+                        <li><a href="../php/manage_order.php">Order</a></li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+    </div>
+
+
+    <!-------main content-->
+    <div class="main-content">
+        <div class="wrapper">
+            <h1>Manage Admin</h1>
+            <br /> <br /> <br />
+
+            <!---butoon to addadminm---->
+            <a href="#" class="btn-primary">Add Admin</a>
+            <br /> <br /> <br />
+
+
+
+
+
+            <table class="tbl-full">
+                <tr>
+                    <th>S.N</th>
+                    <th>Username</th>
+                    <th>Actions</th>
+                </tr>
+                <tr>
+                    <td>1.</td>
+                    <td>Arpana</td>
+                    <td>
+                        <a href="#" class="btn-secondary">Update Admin</a>
+                        <a href="#" class="btn-danger">Delete Admin</a>
+                    </td>
+                </tr>
+                <tr>
+                    <td>2.</td>
+                    <td>Meowwyyy</td>
+                    <td>
+                        <a href="#" class="btn-secondary">Update Admin</a>
+                        <a href="#" class="btn-danger">Delete Admin</a>
+                    </td>
+                </tr>
+                <tr>
+                    <td>3.</td>
+                    <td>Puppyyy</td>
+                    <td>
+                        <a href="#" class="btn-secondary">Update Admin</a>
+                        <a href="#" class="btn-danger">Delete Admin</a>
+                    </td>
+                </tr>
+
+            </table>
+
+        </div>
+    </div>
+
+
+
+
+    <div class="footer">
+        <div class="container">
+            <div class="row">
+                <div class="footer-col-1">
+                    <img src="../../home/images/logo.png">
+                    <h2>Clothing palette's purpose is to provide the latest vintage and soft aesthetic outfits</h2>
+                </div>
+                <div class="footer-col-2">
+                    <h1>Follow Us</h1>
+                    <ul>
+                        <li><a href="#"><i class="fa fa-facebook-official" aria-hidden="true"></i> Facebook</a></li>
+                        <li><a href="#"><i class="fa fa-twitter" aria-hidden="true"></i> Twitter</a></li>
+                        <li><a href="#"><i class="fa fa-instagram" aria-hidden="true"></i> Instagram</a></li>
+                        <li><a href="#"><i class="fa fa-pinterest" aria-hidden="true"></i>Pinterest</a></li>
+                    </ul>
+                </div>
+
+            </div>
+            <hr>
+            <h2 class="copyright">Copyright @ 2024-Clothing Palette</h2>
+        </div>
+    </div>
+</body>
+
+</html>
---
+++ src/features/admin/php/manage_product.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../../common/css/1.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link rel="stylesheet" href="../css/admin.css">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <title>Home page</title>
+</head>
+
+<body>
+    <div class="header">
+        <div class="container">
+            <div class="navbar">
+                <div class="logo">
+                    <img src="../../home/images/logo.png" width="200px">
+                </div>
+                <nav>
+                    <ul id="MenuItems">
+                        <li><a href="../php/admin_homepage.php">Home</a></li>
+                        <li><a href="../php/manage_admin.php">Admin</a></li>
+                        <li><a href="../php/manage_product.php">Products</a></li>
+                        <li><a href="../php/manage_cat.php">Category</a></li>
+                        <li><a href="../php/manage_order.php">Order</a></li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+    </div>
+
+
+<!-----categories----->
+
+<div class="main-content">
+        <div class="wrapper">
+        <h1>Manage Products</h1>
+    </div>
+</div>
+
+
+
+    <div class="footer">
+        <div class="container">
+            <div class="row">
+                <div class="footer-col-1">
+                    <img src="../../home/images/logo.png">
+                    <h2>Clothing palette's purpose is to provide the latest vintage and soft aesthetic outfits</h2>
+                </div>
+                <div class="footer-col-2">
+                    <h1>Follow Us</h1>
+                    <ul>
+                        <li><a href="#"><i class="fa fa-facebook-official" aria-hidden="true"></i> Facebook</a></li>
+                        <li><a href="#"><i class="fa fa-twitter" aria-hidden="true"></i> Twitter</a></li>
+                        <li><a href="#"><i class="fa fa-instagram" aria-hidden="true"></i> Instagram</a></li>
+                        <li><a href="#"><i class="fa fa-pinterest" aria-hidden="true"></i>Pinterest</a></li>
+                    </ul>
+                </div>
+
+            </div>
+            <hr>
+            <h2 class="copyright">Copyright @ 2024-Clothing Palette</h2>
+        </div>
+    </div>
+</body>
+
+</html>
